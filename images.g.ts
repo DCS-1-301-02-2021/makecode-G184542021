f57895c53cfb700e0b99daf2ae9cc77a2a4f15a4
G184542021 <86222381+G184542021@users.noreply.github.com>
2021-07-26 16:29:38 +0900

    ゲーム作り完了しました。

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// 自動生成されたコードです。変更しないでください。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "bogy":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. 2 2 2 2 2 . . . 2 2 . . . . . 
+2 2 1 1 1 2 2 2 . . . . . . . . 
+2 1 1 b b b b 2 2 2 . 2 2 . . . 
+2 1 b b b b 1 1 1 2 . . . . . . 
+2 1 b b b b 1 2 2 2 . 2 2 . . . 
+2 2 1 b b 2 2 2 . . . . . . . . 
+. 2 2 2 2 2 . . . 2 2 . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自動生成されたコードです。変更しないでください。
